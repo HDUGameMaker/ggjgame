918035ce2bc4e63dbd1b01d9276d555804761d77
miruosdasdasdsad <43543644+miruosdasdasdsad@users.noreply.github.com>
2024-01-27 12:07:49 +0800

    1.27merge

--- Assets/GameControler.cs
+++ Assets/GameControler.cs
@@ -1,164 +1,85 @@
-using UnityEngine;
-using TMPro;
-using UnityEditor;
-using UnityEngine.UI;
-using System.Collections.Generic;
 using System;
-using Unity.VisualScripting;
 using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
 
-namespace miruo {
+namespace miruo
+{
     public class GameControler : MonoBehaviour
     {
 
         public TextMeshProUGUI speakText;
-        public GameObject buttonLeft;
-        public GameObject buttonRight;
-        public List<Topic> topics;
+        private List<Topic> topics;
 
+        public float smileToJump;
+
+        public int emoMode;
+        public GameObject playerObj;
+        public Rigidbody playerRig;
+
+        public int moveMode = 1;
         private Topic nowTopic;
         private Coroutine nowTopicCor;
-        private Coroutine nowChosenCor;
         private bool couldGoNextTopic;
         // Start is called before the first frame update
         void Start()
         {
-            buttonLeft.SetActive(false);
-            buttonRight.SetActive(false);
             List<Transform> childList = new List<Transform>();
-
-            FindChild(transform);
-            nowTopic = topics[0];
+            nowTopic = transform.GetChild(0).GetComponent<TopicControler>().mytopic;
             couldGoNextTopic = false;
             nowTopicCor = StartCoroutine(TopicCount());
-            nowChosenCor = StartCoroutine(ChosenCount());
         }
         // Update is called once per frame
         void Update()
         {
-            if (Input.GetKeyDown(KeyCode.R))
-            {
-                //执行右节点
-                if (nowTopicCor != null)
-                {
-                    StopCoroutine(nowTopicCor);
-                }
-                if(nowChosenCor != null)
-                {
-                    StopCoroutine(nowChosenCor);
-                }
-                nowTopic = nowTopic.rightTopic.GetComponent<TopicControler>().mytopic;
-                couldGoNextTopic = false;
-                nowTopicCor = StartCoroutine(TopicCount());
-                nowChosenCor = StartCoroutine(ChosenCount());
-                ChosenHide();
-            }
-            else
-            if (Input.GetKeyDown(KeyCode.L))
-            {
-                //执行左节点
-                if (nowTopicCor != null)
-                {
-                    StopCoroutine(nowTopicCor);
-                }
-                if (nowChosenCor != null)
-                {
-                    StopCoroutine(nowChosenCor);
-                }
-                nowTopic = nowTopic.leftTopic.GetComponent<TopicControler>().mytopic;
-                couldGoNextTopic = false;
-                nowTopicCor = StartCoroutine(TopicCount());
-                nowChosenCor = StartCoroutine(ChosenCount());
-                ChosenHide();
-            }
-            else
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                //执行退出节点
-                if (nowTopicCor != null)
-                {
-                    StopCoroutine(nowTopicCor);
-                }
-                if (nowChosenCor != null)
-                {
-                    StopCoroutine(nowChosenCor);
-                }
-                nowTopic = nowTopic.exitTopic.GetComponent<TopicControler>().mytopic;
-                couldGoNextTopic = false;
-                nowTopicCor = StartCoroutine(TopicCount());
-                nowChosenCor = StartCoroutine(ChosenCount());
-                ChosenHide();
 
-            }
-            else
+
+            //执行持续节点
+            if (couldGoNextTopic)
             {
-                //执行持续节点
-                if (couldGoNextTopic)
+                if(nowTopic.nextTopic != null)
                 {
                     nowTopic = nowTopic.nextTopic.GetComponent<TopicControler>().mytopic;
                     couldGoNextTopic = false;
                     nowTopicCor = StartCoroutine(TopicCount());
-                    if (nowChosenCor != null)
-                    {
-                        StopCoroutine(nowChosenCor);
-                    }
-                    ChosenHide();
-                    nowChosenCor = StartCoroutine(ChosenCount());
                 }
+                else
+                {
+                    KillRobot();
+                }
+                
             }
-
-
         }
+        private void FixedUpdate()
+        {
+            if (moveMode == 1)
+            {
+                playerRig.velocity = Vector3.Lerp(playerRig.velocity, new Vector3(10, 0, 0), 0.02f);
 
+            }
+            if (moveMode == 0)
+            {
+                playerRig.velocity = Vector3.Lerp(playerRig.velocity, Vector3.zero, 0.02f);
+            }
+        }
         IEnumerator TopicCount()
         {
             yield return null;
-            foreach(Textstring nowText in nowTopic.texts)
+            foreach (Textstring nowText in nowTopic.texts)
             {
                 TopicShow(nowText.period);
                 yield return new WaitForSeconds(nowText.time);
             }
             couldGoNextTopic = true;
         }
-        IEnumerator ChosenCount()
-        {
-            if(nowTopic.mychosen.mode == 1)
-            {
-                yield return new WaitForSeconds(nowTopic.mychosen.startTime);
-                ChosenShow();
-                yield return new WaitForSeconds(nowTopic.mychosen.endTime - nowTopic.mychosen.startTime);
-                ChosenHide();
-            }
-        }
         void TopicShow(string ts)
         {
             speakText.text = ts;
         }
-        void ChosenShow()
+        void KillRobot()
         {
-            buttonLeft.SetActive(true);
-            buttonRight.SetActive(true);
-        }
-        void ChosenHide()
-        {
-            buttonLeft.SetActive(false);
-            buttonRight.SetActive(false);
-        }
-        void FindChild(Transform nowTrans)
-        {
-            for(int i = 0; i < nowTrans.childCount; i++)
-            {
-                if (nowTrans.GetChild(i).childCount != 0)
-                {
-                    FindChild(nowTrans.GetChild(i));
-                }
-                TopicControler cacheTopicCon = null;
-                nowTrans.GetChild(i).TryGetComponent<TopicControler>(out cacheTopicCon);
-                if (cacheTopicCon != null)
-                {
-                    topics.Add(cacheTopicCon.mytopic);
-                }
-            }
+
         }
     }
     [Serializable]
